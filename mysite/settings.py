5533f96a0aa315b826e7688de498e0d301e3575c
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2023-12-09 23:49:33 +0600

    [ADD] templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,13 +91,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DEVELOPMENT_MODE = True
 # DEVELOPMENT_MODE = False
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {}
+# if DEVELOPMENT_MODE is True:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 # elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 #     DATABASES = {
 #         'default': {
